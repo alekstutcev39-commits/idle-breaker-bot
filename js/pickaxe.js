f3725242837d13b2e3fb39675ee70645ef5280a6
alekstutcev39-commits <alekstutcev39@gmail.com>
2026-09-14 13:56:41 +0300

    Update pickaxe.js

--- js/pickaxe.js
+++ js/pickaxe.js
@@ -2,60 +2,27 @@ class Pickaxe {
   constructor(x, y, blockSize) {
     this.x = x;
     this.y = y;
-    this.radius = CONFIG.PICKAXE_RADIUS;
+    this.radius = CONFIG.PICKAXE_RADIUS || 14;
 
-    const angle = Utils.randomRange(-Math.PI * 0.8, -Math.PI * 0.2);
-    const speed = CONFIG.PICKAXE_BASE_SPEED;
+    const angle = Utils.randomRange(-Math.PI * 0.7, -Math.PI * 0.3);
+    const speed = CONFIG.PICKAXE_BASE_SPEED || 200;
     this.vx = Math.cos(angle) * speed;
     this.vy = Math.sin(angle) * speed;
 
     this.level = 1;
-    this.maxHp = 320;
-    this.hp = 320;
-    this.baseDamage = CONFIG.PICKAXE_BASE_DAMAGE;
+    this.maxHp = 220;
+    this.hp = 186;
+    this.baseDamage = 10;
     this.alive = true;
     this.absorbed = false;
 
     this.rotation = 0;
-    this.trail = [];
     this.blockSize = blockSize;
-
-    this.updateTier();
+    this.tierColor = '#d4a574'; // Деревянная / Каменная кирка
   }
 
   get damage() {
     return this.baseDamage * this.level;
-  }
-
-  getTier() {
-    const tiers = CONFIG.PICKAXE_TIERS;
-    let tier = tiers[0];
-    for (const t of tiers) {
-      if (this.level >= t.minLevel) tier = t;
-    }
-    return tier;
-  }
-
-  updateTier() {
-    this.tier = this.getTier();
-  }
-
-  upgrade() {
-    this.level++;
-    this.maxHp += 50;
-    this.hp = this.maxHp;
-    this.updateTier();
-  }
-
-  applySpeedBuff() {
-    const mult = 1.5;
-    this.vx *= mult;
-    this.vy *= mult;
-    const speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-    if (speed > CONFIG.MAX_SPEED) {
-      this.vx = (this.vx / speed) * CONFIG.MAX_SPEED;
-      this.vy = (this.vy / speed) * CONFIG.MAX_SPEED;
-    }
   }
 
   update(dt, canvasWidth, worldHeight) {
@@ -65,8 +32,9 @@ class Pickaxe {
     this.x += this.vx * dt;
     this.y += this.vy * dt;
 
-    this.rotation += (Math.abs(this.vx) + Math.abs(this.vy)) * dt * 0.01;
+    this.rotation += (Math.abs(this.vx) + Math.abs(this.vy)) * dt * 0.02;
 
+    // Отскоки от стен
     if (this.x - this.radius < 0) {
       this.x = this.radius;
       this.vx = Math.abs(this.vx) * CONFIG.BOUNCE_DAMPING;
@@ -80,16 +48,6 @@ class Pickaxe {
       this.y = this.radius;
       this.vy = Math.abs(this.vy) * CONFIG.BOUNCE_DAMPING;
     }
-
-    this.trail.push({ x: this.x, y: this.y, alpha: 1 });
-    if (this.trail.length > 8) this.trail.shift();
-    for (const t of this.trail) t.alpha *= 0.85;
-
-    const speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-    if (speed < CONFIG.MIN_SPEED) {
-      this.vx += (Math.random() - 0.5) * 100;
-      this.vy -= Math.random() * 80 + 40;
-    }
   }
 
   bounceOffBlock(block) {
@@ -102,12 +60,8 @@ class Pickaxe {
     this.vx = (this.vx - 2 * dot * normal.x) * CONFIG.BOUNCE_DAMPING;
     this.vy = (this.vy - 2 * dot * normal.y) * CONFIG.BOUNCE_DAMPING;
 
-    const pushForce = this.radius + 2;
-    this.x += normal.x * pushForce * 0.3;
-    this.y += normal.y * pushForce * 0.3;
-
-    this.vx += (Math.random() - 0.5) * 30;
-    this.vy += (Math.random() - 0.5) * 30;
+    this.x += normal.x * 4;
+    this.y += normal.y * 4;
   }
 
   draw(ctx, camera) {
@@ -115,47 +69,25 @@ class Pickaxe {
 
     const pos = camera.worldToScreen(this.x, this.y);
 
-    for (let i = 0; i < this.trail.length; i++) {
-      const t = this.trail[i];
-      const tp = camera.worldToScreen(t.x, t.y);
-      ctx.globalAlpha = t.alpha * 0.3;
-      ctx.fillStyle = this.tier.color;
-      ctx.beginPath();
-      ctx.arc(tp.x, tp.y, this.radius * 0.5, 0, Math.PI * 2);
-      ctx.fill();
-    }
-    ctx.globalAlpha = 1;
-
+    // Отрисовка пиксельной кирки
     ctx.save();
     ctx.translate(pos.x, pos.y);
     ctx.rotate(this.rotation);
 
-    const handleW = this.radius * 0.35;
-    const handleH = this.radius * 1.6;
-    ctx.fillStyle = '#8B6914';
-    ctx.fillRect(-handleW / 2, -handleH / 2, handleW, handleH);
+    // Деревянная рукоять
+    ctx.fillStyle = '#8B4513';
+    ctx.fillRect(-2, -12, 4, 20);
 
-    const headW = this.radius * 1.8;
-    const headH = this.radius * 0.7;
-    ctx.fillStyle = this.tier.color;
-    ctx.fillRect(-headW / 2, -handleH / 2 - headH / 2, headW, headH);
-
-    ctx.fillStyle = 'rgba(255,255,255,0.3)';
-    ctx.fillRect(-headW / 2 + 2, -handleH / 2 - headH / 2 + 2, headW * 0.4, headH * 0.4);
+    // Железное/Каменное лезвие
+    ctx.fillStyle = this.tierColor;
+    ctx.fillRect(-12, -12, 24, 5);
+    ctx.fillRect(-14, -9, 6, 4);
+    ctx.fillRect(8, -9, 6, 4);
 
     ctx.restore();
 
-    const barW = this.radius * 2.5;
-    const barH = 3;
-    const barX = pos.x - barW / 2;
-    const barY = pos.y - this.radius - 8;
-    const hpRatio = this.hp / this.maxHp;
-
-    ctx.fillStyle = 'rgba(0,0,0,0.5)';
-    ctx.fillRect(barX, barY, barW, barH);
-    ctx.fillStyle = '#2ecc71';
-    ctx.fillRect(barX, barY, barW * hpRatio, barH);
-
-    Utils.drawPixelText(ctx, `Lv${this.level}`, pos.x, pos.y + this.radius + 8, 8, '#fff', 'center');
+    // ИНДИКАТОР ЗДОРОВЬЯ КАК НА СКРИНШОТЕ (❤️ 186/220)
+    const hpText = `❤️ ${Math.ceil(this.hp)}/${this.maxHp}`;
+    Utils.drawPixelText(ctx, hpText, pos.x + 18, pos.y - 4, 10, '#ff4444', 'left');
   }
 }
